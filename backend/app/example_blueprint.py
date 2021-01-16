1b422bb062bbb7debbebadc0380a5acbd21d395f
Antoine Daurat <antoinedaurat@gmail.com>
2021-01-16 13:58:39 +0100

    clone and configure h5serv container

--- backend/app/example_blueprint.py
+++ backend/app/example_blueprint.py
@@ -1,6 +1,7 @@
 from bson.json_util import dumps as json_dumps
 from flask import Blueprint, current_app as app
 
+
 bp = Blueprint("hi", __name__)
 
 
@@ -9,6 +10,6 @@ def index():
     db = app.db
     if not db.todos.find_one():
         todos = db.todos
-        todos.insert_one({"name": "say hi!"})
+        todos.insert_one({"message": "hi hi!"})
     todos = db.todos.find_one()
     return json_dumps(todos)
